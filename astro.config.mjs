5072423e6e59c0f0ef9d6d9eb7dbc1fb43bbe18b
GrungeElFz <elf0114_@hotmail.com>
2023-06-02 14:10:55 -0500

    Modified:   Remove Vercel Analytics (astro.config.mjs)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    analytics: true,
-  }),
   site: "https://gee-astro-personal.vercel.app/",
   integrations: [
     mdx(),
