efe7fbfdb72913d18723c9f7a57652aec766e888
GrungeElFz <elf0114_@hotmail.com>
2023-06-25 11:34:07 -0500

    Modified:   Updated site description (src/config.ts)

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Gee's Personal Website";
-export const SITE_DESCRIPTION = "A minimal • personal • digital • corner space to showcase my works with Projects, Playground, and CV ✨";
+export const SITE_DESCRIPTION = "A minimal • personal • digital space to showcase my works with Projects, Playground, Blog, and CV ✨";
